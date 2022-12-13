b3a8ec77c0f8256f830ad458332f922f66903468
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-12-13 15:06:57 -0600

    In this commit: I added functionality to retrieve weather, and output to console.log

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,46 @@
 const searchButton = document.getElementById('searchButton');
 const cityNameInput = document.getElementById('cityNameInput');
-
+//Global Variables
+let history = [];
+let currentWeather = [];
 
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 //5e07344a4e6136949c3131603519df87
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-const getCoordinates = (city) => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=5e07344a4e6136949c3131603519df87`)
+const getWeather = (city) => {
+    //This fetch gets the coordinates
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},US&limit=1&appid=5e07344a4e6136949c3131603519df87`)
     .then( (res) => {
         return res.json();
     })
     .then ( (data) => {
+        return data[0];
+    })
+    .then( (coord) => {
+        let lat = coord.lat.toFixed(2);
+        let lon = coord.lon.toFixed(2);
+        console.log(`Lat: ${lat} and Lon: ${lon}`);
+        //This nested fetch gets the actual weather data
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=5e07344a4e6136949c3131603519df87`)
+
+    })
+    .then( (res) => {
+        return res.json();
+    })
+    .then( (data) => {
         console.log(data);
-        console.log(`${city}: Lat: ${data[0].lat}, Lon: ${data[0].lon}`)
     })
     .catch( (e) => {
         console.log(e);
     })
 }
-searchButton.addEventListener('click', (e) => {
+
+searchButton.addEventListener('click', async (e) => {
     e.preventDefault();
     //Get city's Lat Long
     //Get weather
     let inputCity = cityNameInput.value;
     console.log(`City Entered: ${inputCity}`);
-    getCoordinates(inputCity);
+    let coordinates = await getWeather(inputCity);
+    
 });
