d4876fb0e5abb603744812b687e4da3f8741c933
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-12-13 15:50:49 -0600

    In this commit: Added a guard statement to a load eventListener, in order to prevent loading empty localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,20 @@ const cityNameInput = document.getElementById('cityNameInput');
 let history = [];
 let currentWeather = [];
 
+
+const loadButtons = () => {
+    //Guard against empty local Storage
+    if (localStorage.getItem('history') === null) {
+        console.log('Empty localStorage');
+        return;
+    }
+
+}
+//Onload  get History
+window.addEventListener('load', loadButtons);
+
+
+
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 //5e07344a4e6136949c3131603519df87
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
@@ -17,6 +31,7 @@ const getWeather = (city) => {
         return data[0];
     })
     .then( (coord) => {
+        //Converting to string with only 2 decimal positions 
         let lat = coord.lat.toFixed(2);
         let lon = coord.lon.toFixed(2);
         console.log(`Lat: ${lat} and Lon: ${lon}`);
@@ -35,12 +50,19 @@ const getWeather = (city) => {
     })
 }
 
+const nextF = () => {
+    console.log('Next');
+}
+window.onload = () => {
+    console.log('Loaded');
+}
 searchButton.addEventListener('click', async (e) => {
     e.preventDefault();
     //Get city's Lat Long
     //Get weather
     let inputCity = cityNameInput.value;
     console.log(`City Entered: ${inputCity}`);
-    let coordinates = await getWeather(inputCity);
+    await getWeather(inputCity);
+    await nextF();
     
 });
